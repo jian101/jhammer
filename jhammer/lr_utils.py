ca85c904467239a2ff367af3d4b4876145fc12e8
Dai Jian <daijian1965@outlook.com>
2024-01-29 20:17:47 +0800

    add half_cycle_cosine_after_warmup_lr scheduler

--- jhammer/lr_utils.py
+++ jhammer/lr_utils.py
@@ -1,6 +1,9 @@
 def update_lr(lr: float, optimizer):
     for param_group in optimizer.param_groups:
-        param_group["lr"] = lr
+        if "lr_scale" in param_group:
+            param_group["lr"] = lr * param_group["lr_scale"]
+        else:
+            param_group["lr"] = lr
 
 
 def get_lr(optimizer):
